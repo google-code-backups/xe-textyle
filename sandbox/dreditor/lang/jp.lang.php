b6fe2511c357ff036662d3e168e606a89945450d
heemin <heemin@bebf4043-ad85-6e1f-322f-7830e3d0fd93>
2009-08-04 06:21:09 +0000

    일본어 번역 추가 modules/editor/skins/dreditor/lang/jp.lang.php

---
+++ sandbox/dreditor/lang/jp.lang.php
@@ -0,0 +1,65 @@
+<?php
+	/**
+	 * @file   modules/editor/skins/dreditor/lang/jp.lang.php
+	 * @author sol <sol@ngleader.com>　翻訳：ミニミ
+	 * @brief  段落エディター日本語言語パッケージ
+	 **/
+
+
+	$lang->edit->msg_dr_delete_confirm = '選択した段落を削除して宜しいですか？';
+	$lang->edit->insert_dr_title = '段落のタイトルを入力して下さい。';
+	$lang->edit->richtext_area = 'リッチテキストの入力スペース';
+	$lang->edit->insert_site_name = 'サイト名を入力します。';
+	$lang->edit->insert_explain = '説明の追加入力が出来ます';
+
+
+	$lang->edit->text='テキスト';
+	$lang->edit->text_explain = '「CTRL+ENTER」を押すと段落内の改行が出来ます。';
+	$lang->edit->link = 'リンク';
+	$lang->edit->blockquote = '引用';
+	$lang->edit->insert_blockquote = '引用文を入力して下さい。';
+	$lang->edit->insert_cite = '出所を入力します。 <a>, <strong>, <em> タグの入力も可能です。';
+
+	$lang->edit->image = 'イメージ';
+	$lang->edit->find_image = 'イメージ選択';
+	$lang->edit->uploading = 'アップロード中です。';
+	$lang->edit->uploading_info = '%s MBまでアップロード出来ます。';
+	$lang->edit->uploaded_image = 'ユーザーアップロードイメージ';
+
+	$lang->edit->image_width = 'イメージ幅';
+	$lang->edit->resize = '縮小する';
+	$lang->edit->resize_info = '指定したサイズのコピーを生成し、元のイメージは維持されます。';
+	$lang->edit->resize_error = 'オリジナルイメージより小さいサイズの値を入力して下さい。';
+	$lang->edit->insert_image_explain = 'イメージの説明を入力します。 <a>, <strong>, <em> タグの入力も可能です。';
+
+	$lang->edit->mov ='動画';
+	$lang->edit->insert_mov = '<object> または <embed>に始まるマルチメディアのコードを挿入して下さい。';
+	$lang->edit->insert_mov_explain = '動画の説明を入力します。 <a>, <strong>, <em> タグの入力も可能です。';
+
+	$lang->edit->file='ファイル';
+	$lang->edit->file_select = 'アップロードするファイルを選択して下さい。';
+	$lang->edit->file_uploadinfo = '１個当り %sMB、全体としては %sMBまでアップロードが出来ます。';
+	$lang->edit->file_total ='総<strong class="filecount">{total_filecount}</strong>個';
+	$lang->edit->insert_file_explain ='ファイルの説明を入力します。';
+
+	$lang->edit->hr = '水平線';
+	$lang->edit->hr_simple= '基本水平線';
+
+	$lang->edit->title_title='サブタイトル';
+	$lang->edit->title='タイトル';
+	$lang->edit->title1='タイトル（大サイズ）';
+	$lang->edit->title2='タイトル';
+	$lang->edit->title3='タイトル（小サイズ）';
+
+	$lang->edit->list = 'リスト';
+	$lang->edit->list_explain = '「CTRL+矢印」を押して、項目の位置や段階を変えることが出来ます。';
+	$lang->edit->toc = '目次';
+	$lang->edit->toc_explain ='文書内のサブタイトルをまとめて目次を生成し、自動更新されます。';
+
+	$lang->edit->more = 'もっと見る';
+	$lang->edit->move_button = 'ボタン移動';
+
+    $lang->edit->no_materials = '保存された文材はありません。の説明を入力します。';
+    $lang->edit->msg_no_selected_object = '選択されたものがありません。';
+    $lang->edit->msg_insert_value = '値を入力して下さい。';
+?>
