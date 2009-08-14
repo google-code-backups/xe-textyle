c176539c0731d88023a1994c2fee8266988a0c41
supershop <supershop@bebf4043-ad85-6e1f-322f-7830e3d0fd93>
2009-08-14 00:47:54 +0000

    영어 번역 작업 #1

---
+++ sandbox/material/lang/en.lang.php
@@ -0,0 +1,53 @@
+<?php
+    /**
+     * @file   en.lang.php
+     * @author nicetwo
+     * @brief  글감 수집기의 기본 언어팩(한국어)
+     **/
+
+    $lang->material->popup_title   = '텍스타일 글감수집기';
+    $lang->material->text   = 'Text';
+    $lang->material->images = 'Image';
+    $lang->material->link   = 'Link';
+    $lang->material->list   = 'List';
+    $lang->material->cite   = 'Quote';
+    $lang->material->movie  = 'Movie';
+
+	$lang->material->cmd_deposit = '글감 보관함에 보관하기';
+
+    $lang->material->subject  = 'Subject';
+    $lang->material->about_subject  = 'Please input subject of paragraph.';
+    $lang->material->about_page_subject  = 'Please input subject of link.';
+    $lang->material->content  = 'Content';
+    $lang->material->about_content  = '단락의 본문을 입력해 주세요.';
+
+    $lang->material->about_linksubject  = '링크할 페이지 제목을 입력해 주세요.';
+    $lang->material->about_linkdescription  = '설명을 추가로 입력할 수 있습니다.';
+    $lang->material->description  = 'Description';
+    $lang->material->about_list  = 'Please input list.';
+
+    $lang->material->about_citesubject  = 'Please input quote.';
+    $lang->material->citesubject  = 'Quote';
+    $lang->material->citefrom  = 'Source';
+    $lang->material->about_citefrom  = 'You can input source.';
+
+    $lang->msg_fail_image_save  = '이미지 저장시 오류가 발생하였습니다.';
+    $lang->msg_not_select_image  = '이미지가 선택되지 않았습니다.';
+    $lang->material->select_image  = '이미지 선택';
+    $lang->material->about_imagedescription  = '이미지의 설명을 입력할 수 있습니다.';
+
+    $lang->material->code  = 'Code';
+    $lang->material->about_code  = '<object> 또는 <embed>로 시작하는 멀티미디어 삽입 코드를 넣어주세요.';
+    $lang->material->codedescription  = 'Subtitle or Description';
+    $lang->material->about_codedescription  = '동영상의 설명을 입력할 수 있습니다.';
+
+
+	$lang->material->error_msg_title = "죄송합니다. 오류가 발견되어 지금 북마클릿 기능을 사용하실 수 없습니다. 오류가 발견되는 경우 다음 사항을 확인하세요.";
+	$lang->material->error_msg_list = array(
+		"서버 또는  호스팅 서비스가 현재 정상적으로 제공되고 있는지.",
+		"사이트에서 회원 탈퇴 및 삭제가 되지 않았는지",
+		"북마클릿 링크 오류"
+		);
+
+
+?>
