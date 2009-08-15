cdda88c5bd1d629ade040cfa1af16d4c12d5da40
haneul0318 <haneul0318@bebf4043-ad85-6e1f-322f-7830e3d0fd93>
2009-08-15 04:03:33 +0000

    English translation

--- sandbox/material/lang/en.lang.php
+++ sandbox/material/lang/en.lang.php
@@ -5,24 +5,24 @@
      * @brief  글감 수집기의 기본 언어팩(한국어)
      **/
 
-    $lang->material->popup_title   = '텍스타일 글감수집기';
+    $lang->material->popup_title   = 'Textyle Writing Ingredients Collector';
     $lang->material->text   = 'Text';
     $lang->material->images = 'Image';
     $lang->material->link   = 'Link';
     $lang->material->list   = 'List';
     $lang->material->cite   = 'Quote';
-    $lang->material->movie  = 'Movie';
+    $lang->material->movie  = 'Movie Clip';
 
-	$lang->material->cmd_deposit = '글감 보관함에 보관하기';
+	$lang->material->cmd_deposit = 'Store into Ingredients Storage';
 
     $lang->material->subject  = 'Subject';
-    $lang->material->about_subject  = 'Please input subject of paragraph.';
-    $lang->material->about_page_subject  = 'Please input subject of link.';
+    $lang->material->about_subject  = 'Please input title of paragraph.';
+    $lang->material->about_page_subject  = 'Please input title of link.';
     $lang->material->content  = 'Content';
-    $lang->material->about_content  = '단락의 본문을 입력해 주세요.';
+    $lang->material->about_content  = 'Input content.';
 
-    $lang->material->about_linksubject  = '링크할 페이지 제목을 입력해 주세요.';
-    $lang->material->about_linkdescription  = '설명을 추가로 입력할 수 있습니다.';
+    $lang->material->about_linksubject  = 'Input title of the page to link.';
+    $lang->material->about_linkdescription  = 'You may input additional description.';
     $lang->material->description  = 'Description';
     $lang->material->about_list  = 'Please input list.';
 
@@ -31,22 +31,22 @@
     $lang->material->citefrom  = 'Source';
     $lang->material->about_citefrom  = 'You can input source.';
 
-    $lang->msg_fail_image_save  = '이미지 저장시 오류가 발생하였습니다.';
-    $lang->msg_not_select_image  = '이미지가 선택되지 않았습니다.';
-    $lang->material->select_image  = '이미지 선택';
-    $lang->material->about_imagedescription  = '이미지의 설명을 입력할 수 있습니다.';
+    $lang->msg_fail_image_save  = 'An error occurred while storing image.';
+    $lang->msg_not_select_image  = 'A target image is not selected.';
+    $lang->material->select_image  = 'Select an image';
+    $lang->material->about_imagedescription  = 'You may input description for the image.';
 
     $lang->material->code  = 'Code';
-    $lang->material->about_code  = '<object> 또는 <embed>로 시작하는 멀티미디어 삽입 코드를 넣어주세요.';
+    $lang->material->about_code  = 'Input multimedia insertion code starting with <object> or <embed>.';
     $lang->material->codedescription  = 'Subtitle or Description';
-    $lang->material->about_codedescription  = '동영상의 설명을 입력할 수 있습니다.';
+    $lang->material->about_codedescription  = 'You may input description for the movie clip.';
 
 
-	$lang->material->error_msg_title = "죄송합니다. 오류가 발견되어 지금 북마클릿 기능을 사용하실 수 없습니다. 오류가 발견되는 경우 다음 사항을 확인하세요.";
+	$lang->material->error_msg_title = "Sorry. Bookmarklet feature is not available due to an error. Please, check following possible causes.";
 	$lang->material->error_msg_list = array(
-		"서버 또는  호스팅 서비스가 현재 정상적으로 제공되고 있는지.",
-		"사이트에서 회원 탈퇴 및 삭제가 되지 않았는지",
-		"북마클릿 링크 오류"
+		"Server or Hosting service is not working.",
+		"Your membership is removed from the site",
+		"Bookmarklet link error"
 		);
 
 
