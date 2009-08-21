5f8b483834f90639a5ed1b161f33256466ef764c
supershop <supershop@bebf4043-ad85-6e1f-322f-7830e3d0fd93>
2009-08-21 02:57:31 +0000

    영어 번역 추가

---
+++ sandbox/dreditor/lang/en.lang.php
@@ -0,0 +1,65 @@
+<?php
+	/**
+	 * @file   modules/editor/skins/dreditor/lang/en.lang.php
+	 * @author nicetwo
+	 * @brief  Dr.Editor
+	 **/
+
+
+	$lang->edit->msg_dr_delete_confirm = 'Do you want to remove selected paragraph?';
+	$lang->edit->insert_dr_title = 'Please input title';
+	$lang->edit->richtext_area = 'Richtext Area';
+	$lang->edit->insert_site_name = 'Please input the name of site';
+	$lang->edit->insert_explain = 'Please input description';
+
+
+	$lang->edit->text = 'Text';
+	$lang->edit->text_explain = 'You can continue post with new paragraph by pressing CTRL+ENTER';
+	$lang->edit->link = 'Link';
+	$lang->edit->blockquote = 'Quote';
+	$lang->edit->insert_blockquote = 'Please input quote';
+	$lang->edit->insert_cite = 'You can input the origin source. <a>, <strong>, <em> tags are available.';
+
+	$lang->edit->image = 'Image';
+	$lang->edit->find_image = 'Find Image';
+	$lang->edit->uploading = 'Uploading...';
+	$lang->edit->uploading_info = 'You can upload to maximum %sMB';
+	$lang->edit->uploaded_image = 'User Uploaded Image';
+
+	$lang->edit->image_width = 'Image Width';
+	$lang->edit->resize = 'Resize';
+	$lang->edit->resize_info = 'A new copy of image in inputted size will be created. Original image will still remain';
+	$lang->edit->resize_error = 'Please input smaller size than original image size';
+	$lang->edit->insert_image_explain = 'Please input description. <a>, <strong>, <em> tags are available.';
+
+	$lang->edit->mov ='Movie';
+	$lang->edit->insert_mov = 'Please input multimedia insertion code starts with <object> or <embed>';
+	$lang->edit->insert_mov_explain = 'Please input description. <a>, <strong>, <em> tags are available.';
+
+	$lang->edit->file = 'File';
+	$lang->edit->file_select = 'Please select a file to upload';
+	$lang->edit->file_uploadinfo = 'You can upload to maximum %sMB each, %sMB total';
+	$lang->edit->file_total ='Total <strong class="filecount">{total_filecount}</strong>file(s)';
+	$lang->edit->insert_file_explain ='Please input description';
+
+	$lang->edit->hr = 'HR';
+	$lang->edit->hr_simple= 'Simple HR';
+
+	$lang->edit->title_title = 'Strapline';
+	$lang->edit->title='Subject';
+	$lang->edit->title1='Large';
+	$lang->edit->title2='Medium';
+	$lang->edit->title3='Small';
+
+	$lang->edit->list = 'List';
+	$lang->edit->list_explain = 'You can change the position or level of items by pressing CTRL+Arrow Keys.';
+	$lang->edit->toc = 'Index';
+	$lang->edit->toc_explain ='It makes an index with created straplines, and will be renewed automatically.';
+
+	$lang->edit->more = 'More';
+	$lang->edit->move_button = 'Move Button';
+
+    $lang->edit->no_materials = 'There is no writting ingredient.';
+    $lang->edit->msg_no_selected_object = 'There is no selected object.';
+    $lang->edit->msg_insert_value = 'Please input the value.';
+?>
