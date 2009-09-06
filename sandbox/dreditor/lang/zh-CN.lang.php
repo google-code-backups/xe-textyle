e55e953d04318d5999a9edb529876fb4716d43a1
c2joy <c2joy@bebf4043-ad85-6e1f-322f-7830e3d0fd93>
2009-09-06 02:52:06 +0000

    중국어 번역 - 단락편집기

---
+++ sandbox/dreditor/lang/zh-CN.lang.php
@@ -0,0 +1,65 @@
+<?php
+	/**
+	 * @file   modules/editor/skins/dreditor/lang/zh-CN.lang.php
+	 * @author sol <sol@ngleader.com>    翻译：guny(space.china@gmail.com)
+	 * @brief  段落编辑器简体中文语言包
+	 **/
+
+
+	$lang->edit->msg_dr_delete_confirm = '确定要删除所选锻炼吗？';
+	$lang->edit->insert_dr_title = '请输入段落标题。';
+	$lang->edit->richtext_area = '富编辑器编辑区';
+	$lang->edit->insert_site_name = '在此可以输入网站名称。';
+	$lang->edit->insert_explain = '在此可以输入简单说明。';
+
+
+	$lang->edit->text='文本';
+	$lang->edit->text_explain = '利用CTRL+ENTER组合键可另起段落。';
+	$lang->edit->link = '链接';
+	$lang->edit->blockquote = '引用';
+	$lang->edit->insert_blockquote = '请输入要引用的内容。';
+	$lang->edit->insert_cite = '在此可以输入引用出处(可用HTML标签：<a>, <strong>, <em>)。';
+
+	$lang->edit->image = '图片';
+	$lang->edit->find_image = '上传图片';
+	$lang->edit->uploading = '正在上传图片…';
+	$lang->edit->uploading_info = '上传限制：%s MB';
+	$lang->edit->uploaded_image = '用户上传的图片';
+
+	$lang->edit->image_width = '图片宽度';
+	$lang->edit->resize = '缩小';
+	$lang->edit->resize_info = '创建指定大小的复件并保留原图。';
+	$lang->edit->resize_error = '请输入比原图宽度小的值。';
+	$lang->edit->insert_image_explain = '在此可以输入简单图片说明(可用HTML标签：<a>, <strong>, <em>)。';
+
+	$lang->edit->mov ='视频';
+	$lang->edit->insert_mov = '请输入<object>或<embed>标签开头的视频代码。';
+	$lang->edit->insert_mov_explain = '在此可以输入简单视频描述(可用HTML标签：<a>, <strong>, <em>)。';
+
+	$lang->edit->file='附件';
+	$lang->edit->file_select = '选择上传附件';
+	$lang->edit->file_uploadinfo = '每个附件限制大小：%sMB, 每次上传限制：%sMB';
+	$lang->edit->file_total ='总计<strong class="filecount">{total_filecount}</strong>个';
+	$lang->edit->insert_file_explain ='在此可输入文件说明。';
+
+	$lang->edit->hr = '分割线';
+	$lang->edit->hr_simple= '简单分割线';
+
+	$lang->edit->title_title='内标题';
+	$lang->edit->title='标题';
+	$lang->edit->title1='大标题';
+	$lang->edit->title2='中标题';
+	$lang->edit->title3='小标题';
+
+	$lang->edit->list = '列表';
+	$lang->edit->list_explain = '可利用CTRL+方向键改变项目循序及嵌套级别。';
+	$lang->edit->toc = '目录';
+	$lang->edit->toc_explain ='可以把日志文本内的小标题制成目录，并且每当添加一个内标题此目录就会自动更新。';
+
+	$lang->edit->more = '更多';
+	$lang->edit->move_button = '移动组件';
+
+    	$lang->edit->no_materials = '暂无保存的素材。';
+    	$lang->edit->msg_no_selected_object = '请选择对象。';
+    	$lang->edit->msg_insert_value = '请输入相应的值。';
+?>
