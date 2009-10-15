d4f66d016570ac162f2639eb00ffb3ce786f55ab
royallin <royallin@bebf4043-ad85-6e1f-322f-7830e3d0fd93>
2009-10-15 11:07:50 +0000

    Add Chinese Traditional

---
+++ sandbox/dreditor/lang/zh-TW.lang.php
@@ -0,0 +1,65 @@
+<?php
+	/**
+	 * @file   modules/editor/skins/dreditor/lang/zh-TW.lang.php
+	 * @author sol <sol@ngleader.com> 翻譯：royallin
+	 * @brief  단락에디터 언어팩正體中文語言
+	 **/
+
+
+	$lang->edit->msg_dr_delete_confirm = 'Do you want to remove selected paragraph?';
+	$lang->edit->insert_dr_title = '請輸入標題';
+	$lang->edit->richtext_area = 'Richtext Area';
+	$lang->edit->insert_site_name = '請輸入網站名稱';
+	$lang->edit->insert_explain = '請輸入說明';
+
+
+	$lang->edit->text = 'Text';
+	$lang->edit->text_explain = 'You can continue post with new paragraph by pressing CTRL+ENTER';
+	$lang->edit->link = '連結';
+	$lang->edit->blockquote = '引用';
+	$lang->edit->insert_blockquote = '請輸入引用';
+	$lang->edit->insert_cite = 'You can input the origin source. <a>, <strong>, <em> tags are available.';
+
+	$lang->edit->image = '圖片';
+	$lang->edit->find_image = '尋找圖片';
+	$lang->edit->uploading = '上傳中...';
+	$lang->edit->uploading_info = 'You can upload to maximum %sMB';
+	$lang->edit->uploaded_image = 'User Uploaded Image';
+
+	$lang->edit->image_width = '圖片寬度';
+	$lang->edit->resize = 'Resize';
+	$lang->edit->resize_info = 'A new copy of image in inputted size will be created. Original image will still remain';
+	$lang->edit->resize_error = 'Please input smaller size than original image size';
+	$lang->edit->insert_image_explain = 'Please input description. <a>, <strong>, <em> tags are available.';
+
+	$lang->edit->mov ='影片';
+	$lang->edit->insert_mov = 'Please input multimedia insertion code starts with <object> or <embed>';
+	$lang->edit->insert_mov_explain = 'Please input description. <a>, <strong>, <em> tags are available.';
+
+	$lang->edit->file = '檔案';
+	$lang->edit->file_select = 'Please select a file to upload';
+	$lang->edit->file_uploadinfo = 'You can upload to maximum %sMB each, %sMB total';
+	$lang->edit->file_total ='Total <strong class="filecount">{total_filecount}</strong>file(s)';
+	$lang->edit->insert_file_explain ='Please input description';
+
+	$lang->edit->hr = 'HR';
+	$lang->edit->hr_simple= 'Simple HR';
+
+	$lang->edit->title_title = 'Strapline';
+	$lang->edit->title='Subject';
+	$lang->edit->title1='大';
+	$lang->edit->title2='中';
+	$lang->edit->title3='小';
+
+	$lang->edit->list = '列表';
+	$lang->edit->list_explain = 'You can change the position or level of items by pressing CTRL+Arrow Keys.';
+	$lang->edit->toc = 'Index';
+	$lang->edit->toc_explain ='It makes an index with created straplines, and will be renewed automatically.';
+
+	$lang->edit->more = '更多';
+	$lang->edit->move_button = 'Move Button';
+
+    $lang->edit->no_materials = 'There is no writting ingredient.';
+    $lang->edit->msg_no_selected_object = 'There is no selected object.';
+    $lang->edit->msg_insert_value = 'Please input the value.';
+?>
