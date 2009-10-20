954c979913032f37f6b5ae625b77777a07d41460
ducduydaovn <ducduydaovn@bebf4043-ad85-6e1f-322f-7830e3d0fd93>
2009-10-20 13:20:13 +0000

    Added Vietnamese Language

---
+++ sandbox/dreditor/lang/vi.lang.php
@@ -0,0 +1,65 @@
+<?php
+	/**
+	 * @file   modules/editor/skins/dreditor/lang/ko.lang.php
+	 * @author sol <sol@ngleader.com>
+	 * @brief  단락에디터 언어팩
+	 **/
+
+
+	$lang->edit->msg_dr_delete_confirm = 'Bạn có muốn xóa đoạn văn bản đã chọn?';
+	$lang->edit->insert_dr_title = 'Xin hãy nhập tiêu đề.';
+	$lang->edit->richtext_area = 'Định dạng khu vực viết văn bản';
+	$lang->edit->insert_site_name = 'Tên trang Web.';
+	$lang->edit->insert_explain = 'Mô tả cho trang Web.';
+
+
+	$lang->edit->text='Văn bản';
+	$lang->edit->text_explain = 'Bấm tổ hợp Ctrl+Enter để chuyển sang đoạn văn bản kế tiếp.';
+	$lang->edit->link = 'Liên kết';
+	$lang->edit->blockquote = 'Trích dẫn';
+	$lang->edit->insert_blockquote = 'Hãy nhập văn bản trích dẫn.';
+	$lang->edit->insert_cite = 'Bạn có thể sử dụng mã <a>, <strong>, <em> cho Tag.';
+
+	$lang->edit->image = 'Hình ảnh';
+	$lang->edit->find_image = 'Tìm hình ảnh';
+	$lang->edit->uploading = 'Đang Upload ...';
+	$lang->edit->uploading_info = 'Dung lượng tối đa %s MB';
+	$lang->edit->uploaded_image = 'Người Upload';
+
+	$lang->edit->image_width = 'Độ rộng';
+	$lang->edit->resize = 'Giảm kích thước';
+	$lang->edit->resize_info = 'Có thể giảm kích thước cho phù hợp với giao diện.';
+	$lang->edit->resize_error = 'Hãy nhập kích thước nhỏ hơn kích thước ban đầu.';
+	$lang->edit->insert_image_explain = 'Hãy nhập mô tả cho hình ảnh. Bạn có thể sử dụng <a>, <strong>, <em> cho mô tả này.';
+
+	$lang->edit->mov ='Video';
+	$lang->edit->insert_mov = 'Hãy nhập mã <object> và <embed> cho Media muốn thêm vào bài viết của bạn.';
+	$lang->edit->insert_mov_explain = 'Hãy nhập mô tả cho Media. Bạn có thể sử dụng <a>, <strong>, <em> cho mô tả này.';
+
+	$lang->edit->file='File';
+	$lang->edit->file_select = 'Chọn File muốn Upload';
+	$lang->edit->file_uploadinfo = 'Dung lượng tối đa %sMB/File, Tổng dung lượng được phép Upload %sMB.';
+	$lang->edit->file_total ='Tổng số <strong class="filecount">{total_filecount}</strong> File';
+	$lang->edit->insert_file_explain ='Nhập mô tả cho File.';
+
+	$lang->edit->hr = 'Tách đoạn';
+	$lang->edit->hr_simple= 'Đường kẻ đơn giản';
+
+	$lang->edit->title_title='Tiêu đề';
+	$lang->edit->title='Tiêu đề';
+	$lang->edit->title1='Tiêu đề lớn';
+	$lang->edit->title2='Tiêu đề trung bình';
+	$lang->edit->title3='Tiêu đề nhỏ';
+
+	$lang->edit->list = 'Danh sách';
+	$lang->edit->list_explain = 'Nhấn tổ hợp phím Ctrl+Phím mũi tên, bạn có thể thay đổi vị trí của các mục.';
+	$lang->edit->toc = 'Nội dung';
+	$lang->edit->toc_explain ='Nếu tạo một nhóm các nội dung, tiêu đề của từng đoạn sẽ tự động được tạo.';
+
+	$lang->edit->more = 'Xem thêm';
+	$lang->edit->move_button = 'Chuyển nút bấm';
+
+    $lang->edit->no_materials = 'Không có bài viết được lưu trữ.';
+    $lang->edit->msg_no_selected_object = 'Không có mục nào được chọn.';
+    $lang->edit->msg_insert_value = 'Xin hãy nhập giá trị.';
+?>
