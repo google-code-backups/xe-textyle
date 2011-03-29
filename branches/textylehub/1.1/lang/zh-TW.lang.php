e62b51654dcdc9c5d98389edabd8ef6c91b1559b
ovclas <ovclas@bebf4043-ad85-6e1f-322f-7830e3d0fd93>
2011-03-29 10:05:23 +0000

    textylehub 1.1 create

---
+++ branches/textylehub/1.1/lang/zh-TW.lang.php
@@ -0,0 +1,72 @@
+<?php
+    /**
+     * @file   zh-TW.lang.php
+     * @author ? 翻譯：royallin
+     * @brief  Textyle Hub正體中文語言
+     **/
+
+    $lang->textylehub = 'Textyle Hub';
+    $lang->about_textylehub = "Textyle Hub可顯示最新文章和更新的部落格。\n也可讓有權限的用戶建立自己的部落格。";
+
+    $lang->textylehub_id = 'TextyleHub ID';
+    $lang->textylehub_config = 'Textyle Hub 設定';
+
+    $lang->textyle_creation_type = 'Textyle 建立方式';
+    $lang->about_textyle_creation_type = '可決定使用者建立 Textyle 之後的訪問方式。<br/>Site ID 是用 http://網址/SiteID 所建立的。(使用者輸入的 ID 就是 Site ID )<br/>Domain 是用次級域名(http://domainID.mydomain.net) 的方式建立。(就是使用者輸入的 ID 是 domainID )';
+
+    $lang->newest_documents_count = '最新日誌';
+    $lang->about_newest_documents_count = '可設定最新日誌顯示數量。(預設 20個)';
+
+    $lang->newest_textyles_count = '部落格更新數量';
+    $lang->about_newest_textyles_count = '可設定所顯示的部落格更新數量。(預設 10 個)';
+
+    $lang->textyle_creation_count = '數量限制';
+    $lang->about_textyle_creation_count = '可設定有權限的用戶能建立部落格的數量。(預設 1 個)';
+
+    $lang->sub_newest_textyles_count = '[概要] 部落格更新數量';
+    $lang->about_sub_newest_textyles_count = '可設定所顯示的部落格更新數量。(預設 5 個)';
+
+    $lang->newest_comments_count = '[概要] 最新評論數量';
+    $lang->about_newest_comments_count = '可設定最新評論顯示數量。(預設 5 個)';
+
+    $lang->newest_trackbacks_count = '[概要] 最新引用數量';
+    $lang->about_newest_trackbacks_count = '可設定最新引用顯示數量。(預設 5 個)';
+
+    $lang->cmd_create_textylehub = '建立 Textyle Hub';
+    $lang->about_create_textylehub = '目前沒有 Textyle Hub。 Textyle Hub只能建立一個。';
+
+    $lang->success_create_textylehub = '已建立 Textyle Hub';
+
+    $lang->msg_greeting = '哈摟 <strong>%s</strong>';
+
+    $lang->created_textyle_count = '部落格數量';
+
+    $lang->cmd_create_textyle = '建立部落格';
+
+    $lang->no_textyles = '目前沒有部落格';
+    $lang->no_posts = '目前沒有主題';
+    $lang->no_comments = '目前沒有評論';
+    $lang->no_trackbacks = '目前沒有引用';
+
+    $lang->textyle_list = '部落格列表';
+    $lang->updated_textyle = '最新更新';
+    $lang->newest_posts = '最新主題';
+    $lang->newest_comment = '最新評論';
+    $lang->newest_trackback = '最新引用';
+    $lang->posts = '主題';
+
+    $lang->msg_disable_to_create_textyle = '無法建立部落格';
+
+    $lang->textyle_address = '部落格網址';
+    $lang->blog_title = '部落格標題';
+    $lang->blog_description = '簡介';
+    $lang->textyle_agreement = '使用條款';
+    $lang->cmd_agree = '同意';
+    $lang->about_textyle_agreement = '必須同意使用條款才能建立部落格。';
+    $lang->alert_textyle_address_is_null = '請輸入部落格網址';
+    $lang->alert_textyle_address_is_wrong = '部落格網址長度必須在 4~12 個字元。';
+    $lang->alert_textyle_address_is_exists = '已存在的網址，請輸入新的名稱。';
+    $lang->alert_textyle_address_format = '部落格網址開頭必須是英文字母，並只能用英文和數字。';
+    $lang->alert_textyle_title_is_null = '請輸入部落格標題';
+    $lang->alert_disable_to_create = '無法建立部落格';
+?>
